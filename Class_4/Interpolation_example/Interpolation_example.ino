e8903d65e4ac875dabe8c9de4c70a320193c0968
Dr. Bleep <drbleep@bleeplabs.com>
2020-10-11 15:25:13 -0500

    Update Interpolation_example.ino

--- Class_4/Interpolation_example/Interpolation_example.ino
+++ Class_4/Interpolation_example/Interpolation_example.ino
@@ -11,25 +11,26 @@ void setup() {
 }
 
 void loop() {
-  current_time = millis();
+  current_time = micros(); //read microseconds intead of millseconds
 
   //read and print at one speed
-  if (current_time - prev_time[0] > 20) {
+  if (current_time - prev_time[0] > 20 * 1000) { //20 milliseconds*1000 to make it micros
     prev_time[0] = current_time;
 
     raw_reading = analogRead(A0);
 
     Serial.print(4095); //print this upper value so the auto ranging serial plotter is more clear
-    Serial.print(" ");
+    Serial.print(" "); // print a sapce between values so ther are difernt lines in the plotter
     Serial.print(raw_reading);
     Serial.print(" ");
-    Serial.println(interpolated_output);
+    Serial.println(interpolated_output); //println means new line
 
   }
 
-  //do the interpolation muc hmore quickly
-  if (current_time - prev_time[1] > 1) {
+  //do the interpolation much more quickly
+  if (current_time - prev_time[1] > 250) {
     prev_time[1] = current_time;
+
     if (raw_reading < interpolated_output) {
       interpolated_output--;
     }
