ff036942cc4a3846b8bee424c48770cb2e3c927e
Dr. Bleep <drbleep@bleeplabs.com>
2020-10-11 15:37:33 -0500

    Class 4

--- Class_4/Interpolation_example_-_expo/Interpolation_example_-_expo.ino
+++ Class_4/Interpolation_example_-_expo/Interpolation_example_-_expo.ino
@@ -1,25 +1,29 @@
 // Interpolation info https://github.com/BleepLabs/Arduino-Light-And-Sound/wiki/Interpolation
 
-int raw_reading;
-int interpolated_output;
+float raw_reading = 1;
+float interpolated_output = 1;
 
 unsigned long current_time;
 unsigned long prev_time[8];
-
+float increase_jump_size = 1.001;
+float decrease_jump_size = .995; //it will decrease faster
 
 void setup() {
   analogReadResolution(12); //0-4095 pot values
   analogReadAveraging(32);  //smooth the readings some
+  raw_reading = analogRead(A0) + 1;
+  interpolated_output = raw_reading;
+
 }
 
 void loop() {
   current_time = micros(); //read microseconds intead of millseconds
 
   //read and print at one speed
-  if (current_time - prev_time[0] > 20 * 1000) { //20 milliseconds*1000 to make it micros
+  if (current_time - prev_time[0] > 20* 1000) { //20 milliseconds*1000 to make it micros
     prev_time[0] = current_time;
 
-    raw_reading = analogRead(A0);
+    raw_reading = analogRead(A0) + 1;
 
     Serial.print(4095); //print this upper value so the auto ranging serial plotter is more clear
     Serial.print(" "); // print a sapce between values so ther are difernt lines in the plotter
@@ -30,15 +34,15 @@ void loop() {
   }
 
   //do the interpolation much more quickly
-  if (current_time - prev_time[1] > 250) {
+  if (current_time - prev_time[1] > 500) { //you can make it go much faster, here I've slowed it down so you can see it more easily
     prev_time[1] = current_time;
-
+    
     if (raw_reading < interpolated_output) {
-      interpolated_output--;
+      interpolated_output *= decrease_jump_size;
     }
 
     if (raw_reading > interpolated_output) {
-      interpolated_output++;
+      interpolated_output *= increase_jump_size;
     }
   }
 }
