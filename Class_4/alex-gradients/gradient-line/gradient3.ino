d4e3da3b4434a2d70dfbe4daea75c6b33f13e35b
alex_gallien <alex.gallien@airbnb.com>
2020-10-12 18:28:28 -0700

    Adding AlexG's gradient examples

---
+++ Class_4/alex-gradients/gradient-line/gradient3.ino
@@ -0,0 +1,106 @@
+/* ---------------------------------------------------------------------------
+  ~ Gradient Generator ~
+  
+  * Creates a color gradient from left to right
+  * Right colors set using RGB, controlled by three potentiometers on board
+  * Right colors copied to left by pressing button on board
+   
+--------------------------------------------------------------------------- */
+
+#include <WS2812Serial.h>
+#define num_of_leds 64
+#define led_data_pin 5
+byte drawingMemory[num_of_leds * 3];
+DMAMEM byte displayMemory[num_of_leds * 12];
+WS2812Serial leds(num_of_leds, displayMemory, drawingMemory, led_data_pin, WS2812_GRB);
+float max_brightness = 0.1;
+
+#define left_button_pin 0
+#define top_left_pot_pin A0
+#define top_right_pot_pin A1
+#define bottom_left_pot_pin A2
+#define bottom_right_pot_pin A3
+
+unsigned long current_time;
+unsigned long prev_time[8];
+
+int xy_count;
+int button_state;
+
+// framerate
+int rate1 = 30;
+
+// rate at which rbg values are logged, in ms
+int logging_rate = 250;
+
+// starting left rbg values, right set by pot values
+int left_r = 255;
+int left_g = 0;
+int left_b = 66;
+int right_r;
+int right_g;
+int right_b;
+
+void setup() {
+  leds.begin(); // must be done in setup for the LEDs to work.
+  pinMode(left_button_pin, INPUT_PULLUP); // must be done when reading buttons
+  analogReadResolution(12); // 0-4095 pot values
+  analogReadAveraging(64);  // smooth the readings some
+}
+
+void loop() {
+  current_time = millis();
+
+  if (current_time - prev_time[0] > rate1) {
+    prev_time[0] = current_time;
+
+    // button to copy right to left
+    button_state = digitalRead(left_button_pin);
+    
+    // reads RGB values for right LEDs from pots
+    right_r = analogRead(bottom_left_pot_pin)/16;
+    right_g = analogRead(top_left_pot_pin)/16;
+    right_b = analogRead(bottom_right_pot_pin)/16;
+    
+    // slow logging function bc we don't need that every 30 ms
+    // couldn't find a way to cast int to str so here we are
+    if (current_time - prev_time[1] > logging_rate) {
+      prev_time[1] = current_time;
+
+      Serial.print("Right RGB: (");
+      Serial.print(right_r);
+      Serial.print(",");
+      Serial.print(right_g);
+      Serial.print(",");
+      Serial.print(right_b);
+      Serial.println(")");
+      Serial.print("Left RGB: (");
+      Serial.print(left_r);
+      Serial.print(",");
+      Serial.print(left_g);
+      Serial.print(",");
+      Serial.print(left_b);
+      Serial.println(")");
+    }
+
+    // sweep through LEDs and set color gradient between left and right columns
+    for ( int x_count = 0; x_count < 8; x_count++) {
+      for ( int y_count = 0; y_count < 8; y_count++) {
+        xy_count = x_count + (y_count * 8);  
+        
+        // calculates distance between left/right RGB values and breaks them up into seven chunks
+        leds.setPixelColor(xy_count, left_r + x_count * (right_r - left_r) / 7, 
+                                     left_g + x_count * (right_g - left_g) / 7, 
+                                     left_b + x_count * (right_b - left_b) / 7);
+      }
+    }
+
+    // when button is pressed, copy right LED color to left column
+    if(button_state==0) {
+      left_r = right_r;
+      left_g = right_g;
+      left_b = right_b;
+    }
+    leds.show();
+  }
+}
