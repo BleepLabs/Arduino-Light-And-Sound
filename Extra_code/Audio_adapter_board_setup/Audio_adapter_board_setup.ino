aefa165a1aef84bc5d4aa0e82e1038465fdce2a9
Dr. Bleep <drbleep@bleeplabs.com>
2020-10-30 13:19:14 -0500

    Class 7

---
+++ Extra_code/Audio_adapter_board_setup/Audio_adapter_board_setup.ino
@@ -0,0 +1,122 @@
+//This code shows how to setup the audio adapter board
+
+// The block we copied from the tool is pasted at the top
+// design tool: https://www.pjrc.com/teensy/gui/
+
+// "#include" means add another file to our code
+// So far we've been copy and pasting things in but we can just tell our code to look
+// in a library for more functions and data
+// These are all necessary to get audio working but we don't need to do anything besides include them
+
+#include "smooth.h" //smooth is located in a tab at the top instead of in this file. We just have to include it
+// if the file to be included is in the same directory as the main .ino, use " ". Above we see <> this is for files in other directories.
+
+
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioSynthWaveform       waveform2;      //xy=167,267
+AudioSynthWaveform       waveform1;      //xy=175,215
+AudioMixer4              mixer1;         //xy=383,245
+AudioOutputI2S           i2s1;           //xy=591,244
+AudioConnection          patchCord1(waveform2, 0, mixer1, 1);
+AudioConnection          patchCord2(waveform1, 0, mixer1, 0);
+AudioConnection          patchCord3(mixer1, 0, i2s1, 0);
+AudioConnection          patchCord4(mixer1, 0, i2s1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=484,355
+// GUItool: end automatically generated code
+
+//You can edit the connections by hand but it's easier to do it in the audio library for now.
+
+
+//Then we have our variable declarations like before
+unsigned long cm;
+unsigned long prev[8];
+
+
+void setup() {
+  //there's a lot we need to do in setup now but some of it is just copy paste.
+  // This first group should only be done in setup
+
+  //How much RAM to set aside for the audio library to use.
+  // The audio library uses blocks of a set size so this is not a percentage or kilobytes, just a kind of arbitrary number.
+  // On our Teensy 3.2 we can go up to about 200 but that won't leave any RAM for anyone else.
+  // It's usually the delay and reverb that hog it.
+  AudioMemory(10);
+
+
+  //the control object sgtl5000 is necessary to use the adapter. 
+  // see all of it's functions here https://www.pjrc.com/teensy/gui/?info=AudioControlSGTL5000
+  // it's placed in the tool but not connected to anything
+  sgtl5000_1.enable(); //Turn the adapter board on
+  
+  sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN); //Tell it what input we want to use. 
+  sgtl5000_1.lineInLevel(13); //The volume of the line input. 0-15, the bigger the value the louder the input
+
+  //to use the mic use these instead. Only one can be used, lin or mic 
+  //sgtl5000_1.inputSelect(AUDIO_INPUT_MIC); 
+  //sgtl5000_1.micGain(20); //0-63dB
+  
+
+  //Output volume. Goes from 0.0 to 1.0 but a fully "1.0" signal will clip over .8 or so.
+  // For headphones it's pretty loud at .4
+  // There are lots of places we can change the final volume level. This one you set once and leave alone.
+  sgtl5000_1.volume(0.25);
+  
+  //the hadpone and line outs will always put out the same audio but can have thier audio adjusted
+  // in general it's better to use the line when connecting to mixers, amps, etc, but it's not a huge deal
+  sgtl5000_1.lineOutLevel(29); //13-31, the samller the number the louder the output siganl 
+  
+
+  //This next group can be done anywhere in the code but we want to start things with these
+  // values and change some of them in the loop.
+  
+  //Notice we start by writing the object we want, then a period, then the function
+  // begin(volume from 0.0-1.0 , frequency , shape of oscillator) 
+  // See the tool for more info https://www.pjrc.com/teensy/gui/?info=AudioSynthWaveform
+  waveform1.begin(1, 220.0, WAVEFORM_SINE);
+  waveform2.begin(1, 440.0, WAVEFORM_SINE);
+
+  //The mixer has four inputs we can change the volume of
+  // gain.(channel from 0 to 3, gain from 0.0 to a large number)
+  // A gain of 1 means the output is the same as the input.
+  // .5 would be half volume and 2 would be double
+  // Since we have two oscillators coming in that are already "1" We should take them down by half so we don't clip.
+  // If you go over "1" The top or bottom of the wave is just slammed against a wall
+  mixer1.gain(0, .5);
+  mixer1.gain(1, .5);
+  mixer1.gain(2, 0);
+  mixer1.gain(3, 0);
+
+  //Then we do the stuff we've done before.
+  analogWriteResolution(12); //PWM and A14 DAC output will be 0-4095. This has no effect on the 16 bit in/out of the audio adapter
+  analogReadResolution(12); //AnalogReads will return 0-4095
+  analogReadAveraging(64); 
+
+}
+
+void loop() {
+  cm = millis();
+
+  //We don't have to do anything in the loop since the audio library will jut keep doing what we told it in the setup
+
+  if (cm - prev[0] > 500) {
+    prev[0] = cm;
+
+    //Here we print out the usage of the audio library
+    // If we go over 90% processor usage or get near the value of memory blocks we set aside in the setup we'll have issues or crash.
+    // This pretty much all a copy paste
+    Serial.print("processor: ");
+    Serial.print(AudioProcessorUsageMax());
+    Serial.print("%    Memory: ");
+    Serial.print(AudioMemoryUsageMax());
+    Serial.println();
+    AudioProcessorUsageMaxReset(); //We need to reset these values so we get a real idea of what the audio code is doing rather than just peaking in every half a second
+    AudioMemoryUsageMaxReset();
+
+  }
+}
